b8e740148e8704a68691c72dce254508c19a1333
yama-0107 <46510874+Yuto0107@users.noreply.github.com>
2020-02-01 01:12:54 +0900

    Update model.py

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ class NonLinear(nn.Module):
         super(NonLinear, self).__init__()
         self.input_channels = input_channels
         self.output_channels = output_channels
-        
+
         self.main = nn.Sequential(
             nn.Linear(self.input_channels, self.output_channels),
             nn.ReLU(inplace=True),
             nn.BatchNorm1d(self.output_channels))
-        
+
     def forward(self, input_data):
         return self.main(input_data)
 
@@ -25,7 +25,7 @@ class MaxPool(nn.Module):
         self.num_channels = num_channels
         self.num_points = num_points
         self.main = nn.MaxPool1d(self.num_points)
-    
+
     def forward(self, input_data):
         out = input_data.view(-1, self.num_channels, self.num_points)
         out = self.main(out)
@@ -38,7 +38,7 @@ class InputTNet(nn.Module):
     def __init__(self, num_points):
         super(InputTNet, self).__init__()
         self.num_points = num_points
-        
+
         self.main = nn.Sequential(
             NonLinear(3, 64),
             NonLinear(64, 128),
@@ -48,7 +48,7 @@ class InputTNet(nn.Module):
             NonLinear(512, 256),
             nn.Linear(256, 9)
         )
-        
+
     # shape of input_data is (batchsize x num_points, channel)
     def forward(self, input_data):
         matrix = self.main(input_data).view(-1, 3, 3)
@@ -60,11 +60,10 @@ class InputTNet(nn.Module):
 
 class FeatureTNet(nn.Module):
     def __init__(self, num_points):
-        super(InputTNet, self).__init__()
+        super(FeatureTNet, self).__init__()
         self.num_points = num_points
-        
+
         self.main = nn.Sequential(
-            NonLinear(3, 64),
             NonLinear(64, 64),
             NonLinear(64, 128),
             NonLinear(128, 1024),
@@ -88,10 +87,12 @@ class PointNet(nn.Module):
         super(PointNet, self).__init__()
         self.num_points = num_points
         self.num_labels = num_labels
-        
+
         self.main = nn.Sequential(
             InputTNet(self.num_points),
             NonLinear(3, 64),
+            NonLinear(64, 64),
+            FeatureTNet(self.num_points),
             NonLinear(64, 64),
             NonLinear(64, 128),
             NonLinear(128, 1024),
@@ -102,6 +103,6 @@ class PointNet(nn.Module):
             nn.Dropout(p = 0.3),
             NonLinear(256, self.num_labels),
             )
-        
+
     def forward(self, input_data):
         return self.main(input_data)
